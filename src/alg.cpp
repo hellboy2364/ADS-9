43f9f83e627a27a8dfc2038e71cb3a100f31e22a
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-05-27 13:54:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,22 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+    std::ifstream fin(filename);
+    if (!fin) {
+        std::cout << "File error!" << std::endl;
+        exit(1);
+    }
+    char c;
+    std::string sl;
+    BST<std::string> wood;
+    while (fin.get(c)) {
+        if (isalpha(c)) {
+            sl += tolower(c);
+        } else if (!sl.empty()) {
+            wood.add(sl);
+            sl.clear();
+        }
+    }
+    fin.close();
+    return wood;
 }
